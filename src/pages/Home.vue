5da9c6ed8e2a2655096f310ece48a6d1312fe671
garchomp-game <otoka.japanesedrum@gmail.com>
2019-06-17 23:05:17 +0900

    とりあえず決まっているページ分の外枠だけ作成

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,10 +2,44 @@
 v-ons-page
   v-ons-card
     .title home
+    v-ons-list
+      v-ons-list-item
+        v-ons-button(@click="showCardsIndex") CardsIndex
+      v-ons-list-item
+        v-ons-button(@click="showCardsShow") CardsShow
+      v-ons-list-item
+        v-ons-button(@click="showMypageIndex") MypageIndex
+      v-ons-list-item
+        v-ons-button(@click="showMypageEdit") MypageEdit
+      v-ons-list-item
+        v-ons-button(@click="showSettingIndex") SettingIndex
 </template>
 
 <script>
+import CardsIndex from './cards/index'
+import CardsShow from './cards/show'
+import MypageIndex from './mypage/index'
+import MypageEdit from './mypage/edit'
+import SettingIndex from './setting/index'
+
 export default {
+  methods: {
+    showCardsIndex () {
+      this.$emit('push', CardsIndex)
+    },
+    showCardsShow () {
+      this.$emit('push', CardsShow)
+    },
+    showMypageIndex () {
+      this.$emit('push', MypageIndex)
+    },
+    showMypageEdit () {
+      this.$emit('push', MypageEdit)
+    },
+    showSettingIndex () {
+      this.$emit('push', SettingIndex)
+    }
+  }
 }
 </script>
 
---
+++ src/pages/setting/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-ons-page>
+    <h1>setting/index</h1>
+    <v-ons-button @click="pop">戻る</v-ons-button>
+  </v-ons-page>
+</template>
+<script>
+export default {
+  name: '',
+  data: () => ({
+
+  }),
+  methods: {
+    pop () {
+      this.$emit('pop')
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
