b421532ff020f9a142c6a7b5d87a7b98d3977999
garchomp-game <otoka.japanesedrum@gmail.com>
2019-06-24 09:04:56 +0900

    とりあえず早急にサイズを変更する機能だけ実装

--- src/pages/mypage/edit.vue
+++ src/pages/mypage/edit.vue
@@ -1,63 +1,42 @@
-<template>
-  <v-ons-page>
-    <v-ons-toolbar>
-      <div class="left">
-        <v-ons-toolbar-button @click="pop">戻る</v-ons-toolbar-button>
-      </div>
-      <div class="center">編集</div>
-    </v-ons-toolbar>
-    
-    <v-ons-list>
-      <v-ons-list-item>
-        <v-ons-input placeholder="Trainer Name"></v-ons-input>
-      </v-ons-list-item>
-      <v-ons-list-item>
-        <div class="center">
-          <v-ons-input placeholder="Name" modifier="material"></v-ons-input>
-        </div>
-        <div class="right">
-          <v-ons-switch></v-ons-switch>
-        </div>
-      </v-ons-list-item>
-      <v-ons-list-item>
-        <div class="left">
-          <v-ons-select id="choose-sel">
-            <option value=""></option>
-            <option value="samisigari">さみしがり</option>
-            <option value="ijippari">いじっぱり</option>
-            <option value="yantya">やんちゃ</option>
-          </v-ons-select>
-        </div>
-        <div class="right">
-          <v-ons-switch></v-ons-switch>
-        </div>
-      </v-ons-list-item>
-      <v-ons-list-item>
-        <div class="left">
-          <v-ons-input placeholder="Friend Code(Switch)" modifier="material"></v-ons-input>
-        </div>
-        <div class="right">
-          <v-ons-switch></v-ons-switch>
-        </div>
-      </v-ons-list-item>
-      <v-ons-list-item>
-        <div class="left">
-          <v-ons-input placeholder="Friend Code(Pokemon GO)" modifier="material"></v-ons-input>
-        </div>
-        <div class="right">
-          <v-ons-switch></v-ons-switch>
-        </div>
-      </v-ons-list-item>
-      <v-ons-list-item>
-        <div class="left">
-          <v-ons-input placeholder="Friend Code(3DS)" modifier="material"></v-ons-input>
-        </div>
-        <div class="right">
-          <v-ons-switch></v-ons-switch>
-        </div>
-      </v-ons-list-item>
-    </v-ons-list>
-  </v-ons-page>
+<template lang="pug">
+  v-ons-page
+    v-ons-toolbar
+      .left
+        v-ons-toolbar-button(@click='pop') 戻る
+      .center 編集
+    v-ons-list
+      v-ons-list-item
+        .custom-left
+          v-ons-input.custom-left-input(placeholder='Trainer Name')
+      v-ons-list-item
+        .custom-left
+          v-ons-input.custom-left-input(placeholder='Name', modifier='material')
+        .custom-right
+          v-ons-switch
+      v-ons-list-item
+        .custom-left
+          v-ons-select#choose-sel.custom-left-input
+            option(value='')
+            option(value='samisigari') さみしがり
+            option(value='ijippari') いじっぱり
+            option(value='yantya') やんちゃ
+        .custom-right
+          v-ons-switch
+      v-ons-list-item
+        .custom-left
+          v-ons-input.custom-left-input(placeholder='Friend Code(Switch)', modifier='material')
+        .custom-right
+          v-ons-switch
+      v-ons-list-item
+        .custom-left
+          v-ons-input.custom-left-input(placeholder='Friend Code(Pokemon GO)', modifier='material')
+        .custom-right
+          v-ons-switch
+      v-ons-list-item
+        .custom-left
+          v-ons-input.custom-left-input(placeholder='Friend Code(3DS)', modifier='material')
+        .custom-right
+          v-ons-switch
 </template>
 <script>
 export default {
@@ -72,5 +51,13 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
+<style lang="sass" scoped>
+.custom-left
+  float: left
+  width: 70vw
+  .custom-left-input
+    width: 80%
+.custom-right
+  float: right
+  width: 20vw
 </style>
