037ec2a7dbc14ad182d8483dd114af9263c3e53f
garchomp-game <otoka.japanesedrum@gmail.com>
2019-08-10 21:06:24 +0900

    一旦firebase連帯まで

---
+++ src/const.js
@@ -0,0 +1,18 @@
+const ConfigList = {
+  /**
+   * 性別
+   */
+  sex: [
+    '♂',
+    '♀'
+  ],
+  /**
+   * 性格
+   */
+  character: [
+    'のんびり',
+    'おだやか',
+    'わんぱく'
+  ]
+}
+export default ConfigList
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,68 +3,70 @@ v-ons-page
   Navigation(centerMessage='Home')
   v-ons-list
     v-ons-list-header Trainer Name
-    v-ons-list-item めきは
+    v-ons-list-item {{userList.trainerName}}
     v-ons-list-header Name
-    v-ons-list-item
+    v-ons-list-item {{userList.name}}
     v-ons-list-header Sex
-    v-ons-list-item ♂
+    v-ons-list-item {{calcSex(userList.sex)}}
     v-ons-list-header Character
-    v-ons-list-item のんびり
+    v-ons-list-item {{calcCharacter(userList.character)}}
     v-ons-list-header FriendCode(PokemonGO)
-    v-ons-list-item 1111-2222-3333-4444
+    v-ons-list-item {{userList.frendCodePokemonGO}}
     v-ons-list-header FriendCode(Switch)
-    v-ons-list-item 1111-2222-3333-4444
+    v-ons-list-item {{userList.frendCodeSwitch}}
     v-ons-list-header FriendCode(3DS)
-    v-ons-list-item 1111-2222-3333-4444
+    v-ons-list-item {{userList.frendCode3DS}}
     v-ons-list-header TwitterID
-    v-ons-list-item
+    v-ons-list-item {{userList.twitterId}}
     v-ons-list-header PixivID
-    v-ons-list-item
-  v-ons-input(v-model="word" placeholder="please keyword")
-  v-ons-button(@click="doSend") send
+    v-ons-list-item {{userList.pixivId}}
   v-ons-list
     v-ons-list-item(v-for="(item, index) in testData", :key="index") {{ item.value }}
-  //- v-ons-card
-    .title home
-    //- v-ons-list
-      v-ons-list-item
-        v-ons-button(@click="pushPage('CardsIndex')") CardsIndex
-      v-ons-list-item
-        v-ons-button(@click="pushPage('CardsShow')") CardsShow
-      v-ons-list-item
-        v-ons-button(@click="pushPage('MypageIndex')") MypageIndex
-      //- v-ons-list-item
-        v-ons-button(@click="pushPage('MypageEdit')") MypageEdit
-      //- v-ons-list-item
-        v-ons-button(@click="pushPage('SettingIndex')") SettingIndex
 </template>
 
 <script>
 import firebase from 'firebase/app'
 import 'firebase/database'
+import ConfigList from '../const.js'
 export default {
   data () {
     return {
       testData: [],
-      word: ''
+      userList: {}
     }
   },
-  created () {
-    const refTest = firebase.database().ref('test')
-    refTest.limitToLast(10).on('child_added', this.childAdded)
+  async created () {
+    const refTest = firebase.database().ref('UserData')
+    refTest.limitToLast(10).on('child_added', async (snap) => {
+      const data = await snap.val()
+      console.log(data)
+      this.userList = data
+    })
   },
   methods: {
-    childAdded (snap) {
-      const data = snap.val()
-      this.testData.push({
-        key: snap.key,
-        value: data.value
-      })
+    calcSex (flg) {
+      return ConfigList.sex[flg]
     },
-    doSend () {
-      firebase.database().ref('test').push({
-        value: this.word
-      })
+    calcCharacter (flg) {
+      return ConfigList.character[flg]
+    },
+    async childAdded (snap) {
+      const data = await snap.val()
+      const dataList = {
+        key: snap.key,
+        trainerName: data.trainerName,
+        name: data.name,
+        sex: data.sex,
+        character: data.character,
+        frendCodePokemonGO: data.frendCodePokemonGO,
+        frendCodeSwitch: data.frendCodeSwitch,
+        frendCode3DS: data.frendCode3DS,
+        twitterId: data.twitterId,
+        pixivId: data.pixivId
+      }
+      console.log(dataList)
+      this.userList[0] = await dataList
+      this.userList.splice()
     }
   }
 }
